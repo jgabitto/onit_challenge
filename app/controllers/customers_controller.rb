f7c3925f93f30a0c970f54b53d99f84fedc734d5
jorge gabitto <jgabitto1792@gmail.com>
2023-04-16 19:26:18 -0500

    Modify CustomersController POST route

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -15,9 +15,21 @@ class CustomersController < ApplicationController
     # POST /customers
     def create
         customer = Customer.create!({ name: params[:name], address: params[:address], email: params[:email], phone: params[:phone], registration_date: Date.today, status: "current", notification_type: params[:notification_type]})
-        pet = Pet.create!({ customers: [customer], name: params[:pet_name], birth_date: Date.today, gender: params[:gender], species_id: params[:species_id], breed_id: params[:breed_id], data: nil})
+        pet = Pet.find_by(name: params[:pet_name], birth_date: params[:birth_date], breed_id: params[:breed_id])
+
+        if pet == nil
+            pet = Pet.create!({ customers: [customer], name: params[:pet_name], birth_date: params[:birth_date], gender: params[:gender], species_id: params[:species_id], breed_id: params[:breed_id], data: nil})
+        else
+            pet.customers = [*pet.customers, customer];
+        end
 
         render json: customer, status: :created, include: customer_options
+    end
+
+    # DELETE /customers/:id
+    def destroy
+      @customer.destroy
+      head :no_content
     end
 
     private
--- app/serializers/pet_serializer.rb
+++ app/serializers/pet_serializer.rb
@@ -1,5 +1,5 @@
 class PetSerializer < ActiveModel::Serializer
-  attributes :id, :birth_date, :gender, :data
+  attributes :id, :name, :birth_date, :gender, :data
 
   belongs_to :species
   belongs_to :breed
